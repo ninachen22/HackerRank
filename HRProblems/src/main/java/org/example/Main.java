6856550a8e61216e6aa77b7d3eba455aed05e36c
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2023-02-19 18:37:25 -0500

    pangram problem

--- HRProblems/src/main/java/org/example/Main.java
+++ HRProblems/src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPangram("thisisnotpangram"));
+    }
+
+    // check if string is a pangram
+    public static boolean isPangram(String s) {
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        boolean pangram = true;
+        String lows = s.toLowerCase();
+        for (int i = 0; i < alpha.length(); i++) {
+            if (!lows.contains(String.valueOf(alpha.charAt(i)))) {
+                pangram = false;
+            }
+        }
+        return pangram;
     }
 }
