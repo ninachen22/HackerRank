f6665f99747ac3250ef1ba171e398885509f33e6
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2023-02-19 18:44:49 -0500

    valid pw problem

--- HRProblems/src/main/java/org/example/Main.java
+++ HRProblems/src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPangram("thisisnotpangram"));
+        System.out.println("pangram: " + isPangram("thisisnotpangram"));
+        System.out.println("password: " + validPassword("password"));
+        System.out.println("password: " + validPassword("Pa$sw0rd"));
     }
 
     // check if string is a pangram
@@ -16,5 +18,36 @@ public class Main {
             }
         }
         return pangram;
+    }
+
+    // check if password is valid
+    // reqs are at least 6 characters, no spaces
+    // a lowercase letter, an uppercase letter, special character, and a digit
+    public static boolean validPassword(String S) {
+        boolean lower = false;
+        boolean upper = false;
+        boolean special = false;
+        boolean digit = false;
+        if (S.length() < 6) {
+            return false;
+        }
+        if (S.contains(" ")) {
+            return false;
+        }
+        for (int i = 0; i < S.length(); i++) {
+            if (Character.isLowerCase(S.charAt(i))) {
+                lower = true;
+            }
+            if (Character.isUpperCase(S.charAt(i))) {
+                upper = true;
+            }
+            if (!Character.isDigit(S.charAt(i)) && !Character.isAlphabetic(S.charAt(i))) {
+                special = true;
+            }
+            if (Character.isDigit(S.charAt(i))) {
+                digit = true;
+            }
+        }
+        return lower && upper && special && digit;
     }
 }
