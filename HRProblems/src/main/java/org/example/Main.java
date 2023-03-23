b61bf101f684547afcc03f538067c3e2cdfae501
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2023-03-23 19:18:08 -0400

    testarr add

--- HRProblems/src/main/java/org/example/Main.java
+++ HRProblems/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     static int[] arr = new int[]{3, 6, 4, 1, 2};
+    static Integer[] testArr = {7,1,2,3,4,5};
 
     public static void main(String[] args) {
         System.out.println("pangram: " + isPangram("thisisnotpangram"));
